5e930be3712f45fba2058cc11dc78c6fbde7ab74
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-09 16:40:17 -0300

    alteração de rotas

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://scorpbackend.herokuapp.com",
+  baseURL: "https://scorpfinalbackend.herokuapp.com",
 });
 
 export default api;
