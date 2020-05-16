84397092ab7dc6e2ca84ebc967d532aefd3cd4c2
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-15 22:00:26 -0300

    alteraçoes desfeitas

--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,7 @@ import Cadastro from "./pages/Cadastro";
 export default function Routes() {
   return (
     <BrowserRouter>
-      <Switch basename={"/sub"}>
+      <Switch>
         <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
         <Route path={`${process.env.PUBLIC_URL}/admin`} component={Login} />
         <Route
