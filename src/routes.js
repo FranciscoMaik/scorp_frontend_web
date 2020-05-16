571d7ea9bbdfd4231a9e4c94d7cb31434813310f
FranciscoMaik <chiconunes3081@gmail.com>
2020-05-15 22:27:39 -0300

    public

--- src/routes.js
+++ src/routes.js
@@ -9,12 +9,9 @@ export default function Routes() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path={`${process.env.PUBLIC_URL}/`} exact component={Home} />
-        <Route path={`${process.env.PUBLIC_URL}/admin`} component={Login} />
-        <Route
-          path={`${process.env.PUBLIC_URL}/cadastro`}
-          component={Cadastro}
-        />
+        <Route path="/" exact component={Home} />
+        <Route path="/admin" component={Login} />
+        <Route path="/cadastro" component={Cadastro} />
       </Switch>
     </BrowserRouter>
   );
